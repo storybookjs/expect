36d3c048ee6d5a5cd680167fe9a832cb2291bc3e
Gert Hengeveld <info@ghengeveld.nl>
2021-08-16 12:02:07 +0200

    Extract LICENSE comments and configure Terser

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,38 @@
 const NodePolyfillPlugin = require("node-polyfill-webpack-plugin")
+const TerserPlugin = require("terser-webpack-plugin")
 
 module.exports = {
-  entry: './index.js',
+  entry: "./index.js",
   output: {
     path: __dirname,
-    filename: 'bundle.js',
-    libraryTarget: 'module',
+    filename: "bundle.js",
+    libraryTarget: "module",
   },
   experiments: {
     outputModule: true,
   },
   optimization: {
-    minimize: false,
+    minimizer: [new TerserPlugin({
+      extractComments: {
+        condition: /^\**!|@license/i,
+        filename: 'LICENSE',
+        banner: false,
+      },
+      terserOptions: {
+        ecma: 2020,
+      }
+    })],
+    moduleIds: "named",
+    chunkIds: "named",
   },
-  plugins: [
-    new NodePolyfillPlugin()
-  ],
+  plugins: [new NodePolyfillPlugin()],
   resolve: {
     alias: {
-      'graceful-fs': 'fs'
+      "graceful-fs": "fs",
     },
     fallback: {
       fs: false,
-      module: false
-    }
-  }
-};
+      module: false,
+    },
+  },
+}
